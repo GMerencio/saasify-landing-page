dac1b55af570f241a377ad5345da37f0b8468583
Gabriel Merêncio <gmerencio.santos@gmail.com>
2019-04-08 23:18:17 -0300

    Finished setting up for production

--- webpack.common.js
+++ webpack.common.js
@@ -1,26 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
   entry: [
     './src/js/index.js'
   ],
-  devtool: 'inline-source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
-      {
-       test: /\.(s*)css$/,
-       use: [
-         'style-loader',
-         'css-loader',
-         'postcss-loader',
-         'sass-loader'
-       ]
-     },
      {
        test: /\.(png|svg|jp(e?)g|gif)$/,
        use: [{
